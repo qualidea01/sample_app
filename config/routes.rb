3ccaab9062c1abd725f323d0e4eaa3f2dcc64fa9
qualidea01 <yuto2064@gmail.com>
2020-01-05 21:16:37 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get '/help', to: 'static_pages#help'
 
   get '/about', to: 'static_pages#about'
@@ -6,6 +7,12 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   root 'static_pages#home'
 
