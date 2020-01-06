eedfe2fa1665b96da96606c92a743b4a8d7da257
qualidea01 <yuto2064@gmail.com>
2020-01-07 06:45:40 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -39,6 +43,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  #beforeアクション
 def logged_in_user
   unless logged_in?
     flash[:danger] = "Please log in"
@@ -46,9 +51,18 @@ def logged_in_user
   end
 end
 
-def correct_user
-  @user = User.find(params[:id])
-  redirect_to(root_url) unless @user == current_user
+#ログイン済みユーザーかどうか確認
+def logged_in_user
+  unless logged_in?
+    store_location
+    flash[:danger] = "Please log in"
+    redirect_to login_url
+  end
 end
 
+#正しいユーザーかどうか確認
+def correct_user
+  @user = User.find(params[:id])
+  redirect_to(root_url) unless current_user?(@user)
+end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  test "should redirext index when logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get signup" do
     get signup_path
     assert_response :success
